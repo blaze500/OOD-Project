a38263623df80053123866e91d1c6eda287180d5
blaze500 <57372238+blaze500@users.noreply.github.com>
2020-11-17 00:24:56 -0500

    Update DisplayCards.java

--- DisplayCards.java
+++ DisplayCards.java
@@ -2,24 +2,29 @@ import java.lang.Math;
 
 public class DisplayCards
 {
-
+    // An algorithm that displays the cards
     public DisplayCards(Card[] board)
     {
+        //Rounds the square root of the boards length. This is used to make sure the board is basically a rectangle of X+1 * X-1 length or a square (X*X length).
         int rowSize=(int)Math.round(Math.sqrt(board.length));
+        //These two ints, RangeStart and RangeEnd, print out the cards from Start to End.
         int RangeStart=0;
         int RangeEnd=rowSize;
+        //Basically an overdone for loop, that can can adapt to any board size aside from 0.
         while(true){
-           // System.out.println(board[0].getCard()[0].length);
-            for(int w=0; w<board[0].getCard().length; w++){   // this for loop looks for the length of each row
-                for(int i=RangeStart; i<RangeEnd; i++){ //loop for row X
+               //Loop for the number of rows that make up a card
+            for(int w=0; w<board[0].getCard().length; w++){   
+                  //Prints the whole row of each line of card X
+                for(int i=RangeStart; i<RangeEnd; i++){ 
                     System.out.print(board[i].getCard()[w]);
                     System.out.print("   ");
                 }
                 System.out.println("");
             }
-            
+            //ends the while loop once the board has been printed
             if(RangeEnd==board.length){break;}
             RangeStart+=rowSize;
+            //This is used to make sure that RangeEnd never equals more than the length of the board, so the while loop can end
             RangeEnd=Math.min(RangeEnd+rowSize,board.length);
             System.out.println("");
         }
