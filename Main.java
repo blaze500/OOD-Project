a4e5395619580d7732ebba998060d8cfe594fb47
blaze500 <57372238+blaze500@users.noreply.github.com>
2020-11-17 00:28:17 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 
+//This is more or less a demo right now, just showing that the cards can function properly
+
 public class Main
 {
      public static void main(String[] args) 
     {
         //Current maximum amount of cards is 10
-        ChoosenCards chose= new ChoosenCards(10);
+        ChoosenCards chose= new ChoosenCards(6);
         int[] cards =chose.pickCards();
         CardBoard bordmake= new CardBoard(cards);
         Card[] board =bordmake.MakeBoard();
