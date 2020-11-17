0957595e2da40adccded8de7a016d08772e0763c
blaze500 <57372238+blaze500@users.noreply.github.com>
2020-11-17 00:22:51 -0500

    Update ChoosenCards.java

--- ChoosenCards.java
+++ ChoosenCards.java
@@ -3,7 +3,9 @@ import java.util.LinkedList;
 
 public class ChoosenCards
 {
+    //Stores the ints of the cards that will be gotten from the CardFactory
     int[] cardsNum;
+    //Used to generate random numbers
     Random rand;
     public ChoosenCards(int num)
     {
@@ -12,15 +14,22 @@ public class ChoosenCards
     }
     
     public int[] pickCards(){
+        //This stores numbers that random has already gotten
         LinkedList numbers= new LinkedList();
-       // System.out.println(cards.length);
+        //Used to store how many cards have been placed
         int placed=0;
+        
+        //Does everything while the specified number of cards hasnt been placed
         while(cardsNum.length!=placed){
-            //System.out.println(cards.length);
+            //gets a random number
             int random=getRandom();
+            //Checks to see if the number that was choosen hasnt been picked
             if(!numbers.contains(random)){
+            //Puts the random number in the array of ints
             cardsNum[placed]=random;
+            //adds the random number to the linked list to make sure its not picked again
             numbers.add(random);
+            //Adds 1 to placed, as it has picked 1 card
             placed+=1;
         }    
       }
@@ -28,6 +37,7 @@ public class ChoosenCards
     }
     
     public int getRandom(){
+        //Gets random number
      return rand.nextInt(new CardFactory().NumOfCards());
     }
     
